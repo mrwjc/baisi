c9a6eccd522da5b174de76bbdf14f481cd1057d7
mrwjc <wenjingchang@mrwdemac-mini.lan>
2016-05-04 23:31:53 +0800

    初始化项目

---
+++ 百思/百思/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思
+//
+//  Created by mr.w on 16/5/4.
+//  Copyright © 2016年 mr.w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思/百思/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思
+//
+//  Created by mr.w on 16/5/4.
+//  Copyright © 2016年 mr.w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
